eeaf8c0a7aa5c563e3ed8082e31210aa93fc70f1
Thiago Calmon <thiago.calmon4@gmail.com>
2022-12-24 11:40:06 -0300

    error

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,9 @@ routes.get('/formularioCadastro', (req:Request, res:Response) => {
 routes.get('/public/logoSGCD-removebg-preview.png', (req:Request, res:Response) => {
     res.sendFile(__dirname + '/public/logoSGCD-removebg-preview.png');
   });
+routes.get('/public/css/signUp.css', (req:Request, res:Response) => {
+    res.sendFile(__dirname + '/public/css/signUp.css');
+  });
 routes.post('/cadastroData', new UserController().addUser);
 
 
